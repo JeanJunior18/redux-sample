066c29425b54b33822a61e3ede6c122999c87829
Jean Junior <jclgjunior@gmail.com>
2020-06-01 17:06:12 -0300

    Updates

---
+++ precache-manifest.8af022a4a4ca7151eb7bf6910e717e92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf40936abaf8483daaa2f90221998ead",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a0cfd88b40bffa0a4de",
+    "url": "/static/css/main.6765961a.chunk.css"
+  },
+  {
+    "revision": "4067f1da2f288a775696",
+    "url": "/static/js/2.229a9e3e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.229a9e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0cfd88b40bffa0a4de",
+    "url": "/static/js/main.319431cc.chunk.js"
+  },
+  {
+    "revision": "0b5c9ac70c66225d9e26",
+    "url": "/static/js/runtime-main.fdb1b769.js"
+  }
+]);
